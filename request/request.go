baee7eac23286023fdf6f0b329c73a31dd0d55f2
John Hooks <hooksie11@gmail.com>
2025-03-19 09:51:38 -0400

    update timeout

--- request/request.go
+++ request/request.go
@@ -31,7 +31,7 @@ func (Request) CaddyModule() caddy.ModuleInfo {
 		New: func() caddy.Module {
 			// Default values
 			return &Request{
-				Timeout:     30 * time.Second,
+				Timeout:     60 * time.Second,
 				ServerAlias: "default",
 			}
 		},
